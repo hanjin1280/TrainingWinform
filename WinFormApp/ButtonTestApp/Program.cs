fd0074152564c30b048c6a3d97e2a81fdd8ca361
hanjin1280 <hanjin1280@naver.com>
2020-06-16 11:02:20 +0900

    label 추가

--- WinFormApp/ButtonTestApp/Program.cs
+++ WinFormApp/ButtonTestApp/Program.cs
@@ -16,7 +16,7 @@ namespace ButtonTestApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
