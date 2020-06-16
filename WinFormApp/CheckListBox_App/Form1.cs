b8e36c01423389361d45e0fdd9104e6715a09959
hanjin1280 <hanjin1280@naver.com>
2020-06-16 16:09:17 +0900

    CheckList Test

---
+++ WinFormApp/CheckListBox_App/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckListBox_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var strTemp = "";
+            foreach(var item in checkedListBox1.CheckedItems)
+            {
+                strTemp = strTemp + item.ToString() + " ";
+            }
+            MessageBox.Show($"당신의 취미는 {strTemp}입니다.", "확인", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
---
+++ WinFormApp/ListControlTestApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListControlTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text != "" && listBox1.Items.Contains(textBox1.Text) == false)
+            {
+                listBox1.Items.Add(textBox1.Text);
+                comboBox1.Items.Add(textBox1.Text);
+                textBox1.Text = "";
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedIndex > -1)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+                //listBox1.Items.RemoveAt(listBox1.Items.IndexOf(textBox1.Text));
+                //textBox1.Text = "";
+            }
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label2.Text = comboBox1.SelectedItem.ToString();
+        }
+    }
+}
